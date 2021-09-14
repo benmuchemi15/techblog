6d20383d76fd0a95402e2d878b762ae1aa02daf9
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-09-14 19:31:46 -0400

    connection to sequilize

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const Sequilize = require('sequilize');
+
+require('dotenv').config();
+
+let sequilize;
+
+if (process.env.JAWSDB_URL) {
+    sequilize = new Sequilize(process.env.JAWSDB_URL);
+
+} else {
+    sequilize = new Sequilize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    })
+}
